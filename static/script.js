6f8cb8e3f429cb9042698344eabfc0716c849c31
alberand <albershteyn.andrey@gmail.com>
2020-01-18 19:08:45 +0100

    fix: make links to open in a new tab + change text-decoration

--- static/script.js
+++ static/script.js
@@ -117,7 +117,7 @@ info.update = function (props) {
     var str = ''; 
     for(i = 0; i < data.length; i++){
       var url = data[i]['name'].replace(' ', '+')
-      str = str + '<div class="astr"><a href="https://duckduckgo.com/?q=' + url + '&t=ffab&ia=news&iax=about"> 👨‍🚀 ' + data[i]['name'] + '</a></div>'
+      str = str + '<div class="astr"><a  target="_blank" rel="noopener noreferrer" href="https://duckduckgo.com/?q=' + url + '&t=ffab&ia=news&iax=about"> 👨‍🚀 ' + data[i]['name'] + '</a></div>'
     }
 
     this._div.innerHTML = '<h4>People in space:</h4>' + str
